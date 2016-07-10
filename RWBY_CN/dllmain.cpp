7657f0ac46539eb5e86c97b3fa4467c9616bcc38
nyapl <nyapl@PLUS>
2016-07-10 12:52:58 +0800

    添加项目文件。

---
+++ RWBY_CN/dllmain.cpp
@@ -0,0 +1,145 @@
+#include "CPatch.h"
+#include <windows.h>
+#include <stdio.h>
+#include <vector>
+
+#define Max_Length 665
+
+FILE *TheText = NULL;
+int mono = 0;
+char path[MAX_PATH];
+int titleset = 0;
+std::vector<char *> en;
+std::vector<int> en_len;
+std::vector<char *> zh;
+std::vector<int> zh_len;
+
+
+void ChangeTextProcess(char **Text, int *size)
+{
+	if (!titleset && !strcmp(*Text, "English (United States)"))
+	{
+		SetWindowTextW(FindWindow(NULL, "RWBY: Grimm Eclipse"), L"RWBY: Â¾ÊÞÖ®Ê´");
+		titleset = 1;
+	}
+	if (titleset)
+	{
+		int langt;
+		for (unsigned int i = 0;i < en_len.size();i++)
+		{
+			if (*size == en_len[i])
+			{
+				langt = *size;
+				if (langt > 16)
+					langt = 16;
+				if (!memcmp(*Text, en[i], langt))
+				{
+					*Text = zh[i];
+					*size = zh_len[i];
+					break;
+				}
+			}
+		}
+	}
+}
+
+void ChangeText(int ebp)
+{
+	ChangeTextProcess((char**)(ebp + 8), (int*)(ebp + 0xC));
+}
+
+__declspec(naked) void HOOK_DEBUG_1000135D()
+{
+	__asm
+	{
+		push ebp
+		call ChangeText
+		push ecx
+		push ebx
+		push esi
+		mov esi, [ebp + 0x18]
+		push mono
+		retn
+	}
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL,DWORD fdwReason,LPVOID lpvReserved)
+{
+	switch(fdwReason)
+	{
+		case DLL_PROCESS_ATTACH:
+		{
+			//MessageBox(0, "Ready for debug.", "NyaPlus!", 0);
+			if (GetModuleFileName(0, path, MAX_PATH))
+			{
+				for (int i = strlen(path) - 1; i >= 0; i--)
+				{
+					if (path[i] == '\\' || path[i] == '/')
+					{
+						path[i+1] = 0;
+						strcat_s(path, "RWBY_TEXT.txt");
+						if (fopen_s(&TheText, path, "r+"))
+							break;
+						fseek(TheText, 3, SEEK_SET);
+						char *temptext = new char[Max_Length];
+						char *temp2;
+						int leng;
+						while (!feof(TheText))
+						{
+							fgets(temptext, Max_Length, TheText);
+							leng = strlen(temptext)-1;
+							if (temptext[leng] == '\n')
+								temptext[leng] = 0;
+							else
+								leng++;
+							temp2 = (char *)malloc(leng + 1);
+							memcpy_s(temp2, leng + 1, temptext, leng);
+							en.push_back(temp2);
+							en_len.push_back(leng);
+							fgets(temptext, Max_Length, TheText);
+							leng = strlen(temptext) - 1;
+							if (temptext[leng] == '\n')
+								temptext[leng] = 0;
+							else
+								leng++;
+							temp2 = (char *)malloc(leng + 1);
+							memcpy_s(temp2, leng + 1, temptext, leng);
+							zh.push_back(temp2);
+							zh_len.push_back(leng);
+						}
+						delete[]temptext;
+						fclose(TheText);
+						if (en.size() == zh.size())
+						{
+							mono = (int)GetModuleHandle("mono.dll");
+							CPatch::RedirectJump(mono + 0x135D, &HOOK_DEBUG_1000135D);
+							mono += 0x1363;
+						}
+						break;
+					}
+				}
+			}
+			break;
+		}
+		case DLL_PROCESS_DETACH:
+		{
+			break;
+		}
+		case DLL_THREAD_ATTACH:
+		{
+			break;
+		}
+		case DLL_THREAD_DETACH:
+		{
+			break;
+		}
+	}
+	
+	/* Return TRUE on success, FALSE on failure */
+	return TRUE;
+}
+
+extern "C" void __stdcall RWBY(void)
+{
+	return;
+}
